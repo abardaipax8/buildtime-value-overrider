7f3d8b83702b2c84d83daf5efb144037a7bfcea6
Ahzam Bardai <abardai@pax8.com>
2022-08-19 01:29:25 -0500

    aha!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const core = require('@actions/core')
 
-// const path = core.getInput('filePath')
-// const vals = core.getInput('keyValueObject')
+const path = core.getInput('filePath')
+const vals = core.getInput('keyValueObject')
 
-// console.log(path)
-// console.log(vals)
-
-console.log('hello')
+console.log(path)
+console.log(vals)
